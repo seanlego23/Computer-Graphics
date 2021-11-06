c00ae3500f0eca62e12ba586fb29b7b9863cc1b2
seanlego23 <seanlego23@gmail.com>
2021-11-06 17:58:00 -0400

    Create Line.h

---
+++ Project 1/Source/src/Project/Line.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "model.h"
+
+class Line : public model {
+
+	float lineColor[8]{};
+
+protected:
+
+	float* points = nullptr;
+	unsigned int numOfPoints = 2;
+
+	Line() { }
+
+private:
+
+	void init(glm::vec3 start, glm::vec3 end);
+
+public:
+
+	Line(Material* m, glm::mat4 xForm, glm::vec3 start, glm::vec3 end) {
+		material = m;
+		modelMatrix = xForm;
+
+		init(start, end);
+	}
+
+	virtual ~Line() {
+		if (points)
+			delete[] points;
+	}
+
+	std::pair<float*, unsigned int> getPoints() { return {points, numOfPoints}; }
+
+	virtual glm::vec3 getStartPoint() { return glm::vec3(points[0], points[1], points[2]); }
+
+	virtual glm::vec3 getEndPoint() { return glm::vec3(points[3], points[4], points[5]); }
+
+	virtual void update();
+
+	virtual void setStartPoint(glm::vec3 start) {
+		points[0] = start.x;
+		points[1] = start.y;
+		points[2] = start.z;
+
+		Line::update();
+	}
+
+	virtual void setEndPoint(glm::vec3 end) {
+		points[3] = end.x;
+		points[4] = end.y;
+		points[5] = end.z;
+
+		Line::update();
+	}
+
+	virtual void render(glm::mat4 vMat, glm::mat4 pMat, double deltaTime, SceneGraph* sg);
+
+};
