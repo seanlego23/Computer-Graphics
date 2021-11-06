4f0cf642b0984b21ea64e65b3865242382db2f31
seanlego23 <seanlego23@gmail.com>
2021-11-06 17:55:44 -0400

    Move function to renderer.cpp

--- Project 1/Source/src/Project/renderer.h
+++ Project 1/Source/src/Project/renderer.h
@@ -8,7 +8,10 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include "Material.h"
+#include "SceneGraph.h"
 #include "shader_s.h"
+
+class SceneGraph;
 
 class renderer {
 
@@ -24,6 +27,8 @@ protected:
     glm::mat4 modelMatrix;
 
 public:
+
+    virtual ~renderer() { }
 
     bool isInstanced() {
         return instanced;
@@ -57,43 +62,7 @@ public:
         modelMatrix = glm::scale(modelMatrix, glm::vec3(scale[0], scale[1], scale[2]));
     }
 
-    virtual void render(glm::mat4 vMat, glm::mat4 pMat, double deltaTime) { // here's where the "actual drawing" gets done
-
-        glm::mat4 mvp;
-
-        unsigned int shaderID = material->use();
-
-        //rotate(glm::value_ptr(glm::vec3(0.0f, 0.0f, 1.0f)), deltaTime); // easter egg!  rotate incrementally with delta time
-
-        mvp = pMat * vMat * modelMatrix;
-
-        glUniformMatrix4fv(glGetUniformLocation(shaderID, "m"), 1, GL_FALSE, glm::value_ptr(modelMatrix));
-        glUniformMatrix4fv(glGetUniformLocation(shaderID, "v"), 1, GL_FALSE, glm::value_ptr(vMat));
-        glUniformMatrix4fv(glGetUniformLocation(shaderID, "p"), 1, GL_FALSE, glm::value_ptr(pMat));
-
-        glUniformMatrix4fv(glGetUniformLocation(shaderID, "mvp"), 1, GL_FALSE, glm::value_ptr(mvp));
-        myShader->setInt("instances", instance_count);
-        myShader->setVec4("material.ambient", material->ambient.r, material->ambient.g, material->ambient.b, material->ambient.a);
-        myShader->setVec4("material.diffuse", material->diffuse.r, material->diffuse.g, material->diffuse.b, material->diffuse.a);
-        myShader->setVec4("material.specular", material->specular.r, material->specular.g, material->specular.b, material->specular.a);
-        myShader->setFloat("material.shininess", material->shininess);
-        if (material->textureID) { //If textureID != 0
-            glBindTexture(GL_TEXTURE_2D, material->textureID);
-            myShader->setInt("material.texture", 0);
-        }
-        myShader->setDouble("elapsedTime", elapsedTime += deltaTime);
-
-        glEnable(GL_CULL_FACE);
-        glCullFace(GL_BACK);
-        glFrontFace(GL_CCW);
-
-        glBindVertexArray(VAO);
-
-        if (!instanced)
-            glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
-        else 
-            glDrawElementsInstanced(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0, instance_count);
-    }
+    virtual void renderer::render(glm::mat4 vMat, glm::mat4 pMat, double deltaTime, SceneGraph* sg);
 
     virtual void OnTransform(glm::mat4 oldModel, glm::mat4 newModel) { }
 };
