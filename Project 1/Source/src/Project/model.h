2a16dc311aca24851fb4d937288a35c892fcc329
seanlego23 <seanlego23@gmail.com>
2021-11-06 17:56:09 -0400

    Update model.h

--- Project 1/Source/src/Project/model.h
+++ Project 1/Source/src/Project/model.h
@@ -1,28 +1,68 @@
 #pragma once
 
-#include <map>
+#include <string>
+#include <set>
 
+#include "attachment.h"
 #include "renderer.h"
+
+class transformEvent : public attachmentEvent {
+
+	glm::mat4 oldXForm;
+	glm::mat4 newXForm;
+
+protected:
+	
+	static const std::string type;
+
+public:
+
+	transformEvent(const attachment* a, glm::mat4 oldModelMat, glm::mat4 newModelMat) : attachmentEvent(a), 
+		oldXForm(oldModelMat), newXForm(newModelMat) { }
+
+	virtual std::string getType() const { return type; }
+
+	virtual auto getFunctionCall() const -> void(model::*)(attachmentEvent*);
+
+	glm::mat4 getOldXForm() { return oldXForm; }
+
+	glm::mat4 getNewXForm() { return newXForm; }
+};
 
 extern unsigned long modelIDCounter;
 
 class model : public renderer { 
-
 protected:
 
-	std::map<unsigned long, model*> attachedModels;
+	std::set<attachment, bool(*)(const attachment&, const attachment&)> attachedModels;
 
 public:
 
 	const unsigned long modelID;
 
-	model() : modelID(++modelIDCounter) { }
+	model() : modelID(++modelIDCounter), attachedModels(&attachment::compare) { }
+
+	virtual ~model() { }
 
 	virtual void update() { }
 
-	void attachModel(model* m);
+	attachment* attachModel(model* m);
 
-	void detachModel(model* m);
+	bool detachModel(model* m);
+
+	virtual void onAttachmentEvent(attachmentEvent* event) { }
+
+	virtual void onTransformEvent(attachmentEvent* event) { }
+
+	attachment* getModelAttachment(model* m);
+
+	attachment* getModelAttachment(unsigned long id);
+
+	virtual void OnTransform(glm::mat4 oldModel, glm::mat4 newModel) { 
+		for (const attachment& a : attachedModels) {
+			if (a.onTransform)
+				attachmentEvent::events.push(std::unique_ptr<attachmentEvent>(new transformEvent(&a, oldModel, newModel)));
+		}
+	}
 
 };
-
