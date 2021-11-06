fc263f40b3353248a77a5ab34903b3eb328f83e6
seanlego23 <seanlego23@gmail.com>
2021-11-06 17:54:32 -0400

    Create textures.h

---
+++ Project 1/Source/src/Project/textures.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <glad/glad.h>
+
+#include "stb_image.h"
+
+unsigned int loadTexture(const char* file) {
+	unsigned int temp;
+	int width, height, nrChannels;
+
+	glGenTextures(1, &temp);
+
+    glBindTexture(GL_TEXTURE_2D, temp);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    unsigned char* data = stbi_load(file, &width, &height, &nrChannels, 0);
+
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+
+    stbi_image_free(data);
+
+    return temp;
+}
