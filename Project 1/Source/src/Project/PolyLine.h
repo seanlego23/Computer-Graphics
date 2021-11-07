082c35a1cc9ca2d7cc9c95243efafe526b4c8203
seanlego23 <seanlego23@gmail.com>
2021-11-07 09:35:48 -0500

    Add constructors and getPoint method

--- Project 1/Source/src/Project/PolyLine.h
+++ Project 1/Source/src/Project/PolyLine.h
@@ -1,8 +1,26 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 #include "Line.h"
+#include "Material.h"
 
 class PolyLine : public Line { 
+
+public:
+
+	PolyLine() = delete;
+
+	PolyLine(const PolyLine& rhs);
+
+	PolyLine(const PolyLine& rhs, Material* m, glm::mat4 xForm);
+
+	PolyLine(Material* m, glm::mat4 xForm, unsigned int nPoints, glm::vec3 pts...);
+
+	glm::vec3 getPoint(unsigned int index) {
+		assert(index >= 0 || index < this->getNumOfPoints());
+		return glm::vec3(points[index*3], points[index*3+1], points[index*3+2]);
+	}
 
 };
 
