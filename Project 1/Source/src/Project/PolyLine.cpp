dc901404f34d1ecb9e67c3454228748eb729fe3d
seanlego23 <seanlego23@gmail.com>
2021-11-07 19:09:12 -0500

    Define constructors

--- Project 1/Source/src/Project/PolyLine.cpp
+++ Project 1/Source/src/Project/PolyLine.cpp
@@ -1,1 +1,8 @@
 #include "PolyLine.h"
+
+PolyLine::PolyLine(Material* m, glm::mat4 xForm, unsigned int nPoints, glm::vec3 pts ...) : Line() { 
+	material = m;
+	modelMatrix = xForm;
+}
+
+PolyLine::PolyLine(const PolyLine& rhs) : Line(rhs) { }
